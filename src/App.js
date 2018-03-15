d68cf0228db3e171eb58e80061cc836df2efdde2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-15 22:00:18 +0900

    #2 Return Types Strings and Fragments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Fragment>
+        <ReturnTypes />
+      </Fragment>
+    );
   }
 }
 
