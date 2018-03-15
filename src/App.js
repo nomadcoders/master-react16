b22bd289bd18fc8448907ec8f673dbd941f215b1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-15 22:08:45 +0900

    #3 Portals

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
+}
+
+const Message = () => "Just touched it!";
 
 class ReturnTypes extends Component {
   render() {
@@ -11,6 +20,7 @@ class App extends Component {
     return (
       <Fragment>
         <ReturnTypes />
+        <Portals />
       </Fragment>
     );
   }
